ffb2133027d2fae185f4e36931f83f9620790ca3
Smart <maanukyanarman@gmail.com>
2022-12-20 15:12:04 +0400

    Homework

---
+++ src/chapter3/ClassAutoArray.java
@@ -0,0 +1,9 @@
+package chapter3;
+
+public class ClassAutoArray {
+    public static void main(String[] args) {
+
+        int month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("v aprl " + month_days[3] + "dnei");
+    }
+}
